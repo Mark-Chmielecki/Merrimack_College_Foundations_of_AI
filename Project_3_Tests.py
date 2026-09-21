f0c06f20b0839246b53cae8daccad12abe38f274
Mark-Chmielecki <mark.chmielecki@gmail.com>
2026-09-21 08:49:51 -0400

    Project_3_The_Smart_Navigator.p

---
+++ Project_3_Tests.py
@@ -0,0 +1,21 @@
+import heapq
+from collections import deque
+
+def main():
+
+    list = deque([1, 2, 3])
+    while list:
+        #print(list.popleft())
+        print(list.pop())
+        for n in 
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
--- Project_3_The_Smart_Navigator.py
+++ Project_3_The_Smart_Navigator.py
@@ -6,6 +6,7 @@ Date: September 21, 2026
 
 import heapq
 from collections import deque
+from tracemalloc import start
 
 def get_neighbors(node, grid):
     """Returns valid North, South, East, West neighbors (0 = path, 1 = wall)."""
@@ -22,8 +23,10 @@ def manhattan_distance(a, b):
     TASK: Implement Manhattan Distance h(n)
     Formula: |x1 - x2| + |y1 - y2|
     """
-    # TODO: Your code here
-    return 0
+    x = abs(a[0] - b[0])    # Calculate absolute difference in x coordinates 
+    y = abs(a[1] - b[1])    # Calculate absolute difference in y coordinates
+
+    return x + y            # Return sum of absolute differences in x and y coordinates, which is the Manhattan distance between points a and b
 
 def breadth_first_search(grid, start, goal):
     """
@@ -32,8 +35,22 @@ def breadth_first_search(grid, start, goal):
     - Return the total count of nodes visited.
     """
     nodes_visited = 0
-    # TODO: Your code here
-    return nodes_visited
+    frontier = deque([start])                                   # Initialize the frontier with the start node
+    visited_nodes = {start}                                     # Initialize the set of visited nodes
+
+    while frontier:
+        current = frontier.popleft()                            # Remove the current note from the frontier in FIFO order
+        nodes_visited += 1                                      # Increment the count of nodes visited
+
+        if current == goal:                                     # If the current node is the goal, break out of the loop
+            break
+
+        for neighbor in get_neighbors(current, grid):           # Get the neighbors of the current node, only zeroes (paths) are valid neighbors, 1's are walls
+            if neighbor not in visited_nodes:                   # If the neighbor has been visited, skip it, otherwise add it to the visited nodes and add it to the frontier
+                visited_nodes.add(neighbor)                     # Add the neighbor to the visited nodes set
+                frontier.append(neighbor)                       # Add zero neighbors to the frontier for future exploration
+
+    return nodes_visited                                        # Return the total count of nodes visited
 
 def a_star_search(grid, start, goal):
     """
@@ -42,11 +59,30 @@ def a_star_search(grid, start, goal):
     - Use the tie-breaker: priority = (g + h) + (h * 0.001)
     - Return the total count of nodes visited.
     """
-    nodes_visited = 0
-    # TODO: Your code here
+    nodes_visited = 0   
+    frontier = []                                                               # Initialize frontier
+    heapq.heappush(frontier, (0, 0, start))                                     # Pushes priority = 0, g/cost = 0, and start node onto the frontier heap
+    #print (f"frontier {frontier}")                                           # Print the start and goal nodes for debugging
+    visited_nodes = {start}                                                     # Initialize the set of visited nodes with start (0,0)
+
+    while frontier:
+        print (f"frontier {frontier}") 
+        current_priority, current_g, current_node = heapq.heappop(frontier)     # Remove current node from frontier in priority order 
+        nodes_visited += 1                                                              # Increment the count of nodes visited
+
+        if current_node == goal:                                                # If the current node is the goal, break out of the loop
+            break
+
+        for neighbors in get_neighbors(current_node, grid):                      # Get the neighbors of the current node, only zeroes (paths) are valid neighbors, 1's are walls
+            if neighbors not in visited_nodes:                                   # If the neighbor has been visited, skip it, otherwise add it to the visited nodes and add it to the frontier
+                visited_nodes.add(neighbors)                                     # Add the neighbor to the visited nodes set
+                g = current_g + 1                                               # 
+                #print (f"the g is {g}")                                               # Increment the cost to reach the neighbor (g(n) = g(current) + 1)
+                h = manhattan_distance(neighbors, goal)                          # Calculate the heuristic cost to reach the goal from the neighbor (h(n) = Manhattan distance)
+                priority = (g + h) + (h * 0.001)                                # Calculate the priority of the neighbor using the tie-breaker formula
+                heapq.heappush(frontier, (priority, g, neighbors))               # Add the neighbor to the frontier with its priority, g value, and coordinates
+
     return nodes_visited
-
-
 
 def main():
     start = (0, 0)
@@ -76,11 +112,9 @@ def main():
 # TEST EXECUTION
 # ==========================================================
     
-    
-
     print(f"1. WINDING MAZE - BFS: {breadth_first_search(m1, start, (9,9))} | A*: {a_star_search(m1, start, (9,9))}")
-    print(f"2. CHECKERBOARD - BFS: {breadth_first_search(m2, start, (9,9))} | A*: {a_star_search(m2, start, (9,9))}")
-    print(f"3. THE SPIRAL   - BFS: {breadth_first_search(m3, start, (6,6))} | A*: {a_star_search(m3, start, (6,6))}")
+   # print(f"2. CHECKERBOARD - BFS: {breadth_first_search(m2, start, (9,9))} | A*: {a_star_search(m2, start, (9,9))}")
+   # print(f"3. THE SPIRAL   - BFS: {breadth_first_search(m3, start, (6,6))} | A*: {a_star_search(m3, start, (6,6))}")
 
 if __name__ == "__main__":
     main()
